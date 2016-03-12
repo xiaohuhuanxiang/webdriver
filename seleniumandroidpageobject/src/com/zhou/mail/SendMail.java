6ca7e63775236d66df6a1d55620c900491443e8d
apple <apple@appledeMacBook-Air.local>
2016-03-12 12:32:19 +0800

    webdriver

---
+++ seleniumandroidpageobject/src/com/zhou/mail/SendMail.java
@@ -0,0 +1,35 @@
+package com.zhou.mail;
+
+import com.zhou.htmlutil.HtmlToString;
+
+public class SendMail {
+	//String path=this.getClass().getClassLoader().getResource("").getPath();///C:/Users/zhoupp/workspace/seleniumandroid/bin/
+
+	public void sendMail(){
+		
+		 MailInfo mailInfo = new MailInfo();  
+	        mailInfo.setMailServerHost("smtp.126.com");  
+	        mailInfo.setMailServerPort("25");  
+	        mailInfo.setValidate(true);  
+	        mailInfo.setUsername("zpphnkjxy@126.com");  
+	        mailInfo.setPassword("ZPPzpp123");// 您的邮箱密码  
+	        mailInfo.setFromAddress("zpphnkjxy@126.com");  
+	        mailInfo.setToAddress("819669450@qq.com");  //zhoupp@fun.tv
+	        mailInfo.setSubject("autotest reporter");  
+	        String szFileName ="C:/Users/zhoupp/workspace/seleniumandroidpageobject/test-output/emailable-report2.html";        
+	       // String szFileName = "/test-output/emailable-report2.html";
+	        
+	        
+	        //附件  
+	        String[] attachFileNames={"C:/Users/zhoupp/workspace/seleniumandroidpageobject/test-output/emailable-report2.html"};  
+	        mailInfo.setAttachFileNames(attachFileNames);  
+	        mailInfo.setContent(HtmlToString.openFile(szFileName));  
+	        SimpleMail.sendHtmlMail(mailInfo);// 发送html格式  
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SendMail sendMail =new SendMail();
+		sendMail.sendMail();
+	}
+
+}
